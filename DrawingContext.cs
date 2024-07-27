06e97fedeeb49f28eeaeb869072fd961714cbd64
Kato <1011gyor@gmail.com>
2024-07-27 20:36:51 +0900

    Additional Origin

---
+++ DrawingContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace New_XPPen_Auxiliary_Driver
+{
+    //paintEventArgsを受け取って描画する
+    internal class DrawingContext
+    {
+        virtual public void Draw(ref PaintEventArgs paintEventArgs)
+        {
+
+        }
+    }
+    internal class DrawingContextLine : DrawingContext
+    {
+        public DrawingContextLine()
+        {
+
+        }
+        override public void Draw(ref PaintEventArgs paintEventArgs)
+        {
+
+        }
+    }
+}
